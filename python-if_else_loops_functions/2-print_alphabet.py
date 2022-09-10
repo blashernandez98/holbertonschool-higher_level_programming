ca6b53060a596452f8574c382387ac62dd14007f
Blas <blashernandez98@gmail.com>
2022-09-10 11:54:32 -0700

    Task2fix

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-for letter in alphabet:
-    print(f"{letter}", end="")
+for letter in range(97, 123):
+    print("{:c}".format(letter), end="")
