3b91ce978cfaffedb71b19af3e7752e94f2d9d31
Blas <blashernandez98@gmail.com>
2022-09-11 07:24:29 -0700

    Task6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,5 +3,6 @@ for first_d in range(0, 9):
     for second_d in range(1, 10):
         if (first_d == 8):
             continue
-        print(f"{first_d}{second_d}", end=", ")
-print(f"{first_d}{second_d}")
+        print("{:d}".format(first_d), end="")
+        print("{:d}".format(second_d), end=", ")
+print("89")
