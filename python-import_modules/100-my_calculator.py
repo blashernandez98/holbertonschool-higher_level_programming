528b3fd928f22ceb3f973b8bce1d2af42135ff2b
Blas <blashernandez98@gmail.com>
2022-09-11 19:51:31 -0700

    Task100

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -2,13 +2,13 @@
 if (__name__ == '__main__'):
     from calculator_1 import *
     import sys
+    if (argc != 4):
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        quit(1)
     argv = sys.argv
     argc = len(argv)
     a = int(argv[1])
     b = int(argv[3])
-    if (argc != 4):
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        quit(1)
     operators = [["+", add], ["-", sub], ["*", mul], ["/", div]]
     for i in range(0, 4):
         if (argv[2] == operators[i][0]):
