d95a66a048bfd24fece1b303755d0c30e9e980ac
Blas <blashernandez98@gmail.com>
2022-09-11 19:54:54 -0700

    Task100

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -10,9 +10,9 @@ if (__name__ == '__main__'):
     a = int(argv[1])
     b = int(argv[3])
     operators = [["+", add], ["-", sub], ["*", mul], ["/", div]]
-    for i in range(0, 4):
-        if (argv[2] == operators[i][0]):
-            print("{:d} {} {:d} = {:d}".format(a, operators[i][0], b, operators[i][1](a, b)))
+    for op in operators:
+        if (argv[2] == op[0]):
+            print("{:d} {} {:d} = {:d}".format(a, op[0], b, op[1](a, b)))
             quit(0)
     print("Unknown operator. Available operators: +, -, * and /")
     quit(1)
