3e11aa0134ce3fa0c09660fe30ac3a61541c7439
Blas <blashernandez98@gmail.com>
2022-09-11 19:48:16 -0700

    Task100

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+if (__name__ == '__main__'):
+    from calculator1 import *
+    import sys
+    argv = sys.argv
+    argc = len(argv)
+    a = int(argv[1])
+    b = int(argv[3])
+    if (argc != 4):
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        quit(1)
+    operators = [["+", add], ["-", sub], ["*", mul], ["/", div]]
+    for i in range(0, 4):
+        if (argv[2] == operators[i][0]):
+            print("{:d} {} {:d} = {:d}".format(a, operators[i][0], b, operators[i][1](a, b)))
+            quit(0)
+    print("Unknown operator. Available operators: +, -, * and /")
+    quit(1)
