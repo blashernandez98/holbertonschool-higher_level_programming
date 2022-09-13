861088c41627d13b91580029cd773db374960499
Blas <blashernandez98@gmail.com>
2022-09-13 16:13:32 -0700

    Task7

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def add_tuple(a=(), b=()):
+    c = [0, 0]
+    for i in range(0, 2)
+        if (a is not None):
+            if (a[i] is not None):
+                c[i] = a[i]
+        if (b is not None):
+            if (b[i] is not None):
+                c[i] += b[i]
+    return (tuple(c))
