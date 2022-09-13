5c97cd127c5bec4eedf95d700c24fe42479d891c
Blas <blashernandez98@gmail.com>
2022-09-13 16:20:57 -0700

    Task8

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    length = 0
+    first = None
+    if (sentence is not None):
+        length = len(sentence)
+        if (length > 0):
+            first = sentence[0]
+    return (tuple(length, first))
