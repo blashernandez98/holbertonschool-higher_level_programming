de2c068f0fe8df089c34189df3e9e64268a7576b
Blas <blashernandez98@gmail.com>
2022-09-13 11:46:56 -0700

    Pycodestyle

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def print_matrix_integer(matrix = [[]]):
+def print_matrix_integer(matrix=[[]]):
     if (matrix is not None):
         for row in matrix:
             for i in range(len(row)):
