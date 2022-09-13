13202c7c6c3f3f2d3ffb3274907e4e836f643ba8
Blas <blashernandez98@gmail.com>
2022-09-13 11:44:45 -0700

    Task6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,8 @@
 def print_matrix_integer(matrix = [[]]):
     if (matrix is not None):
         for row in matrix:
-            for element in row:
-                print("{:d}".format(element), end=" ")
-            print()
+            for i in range(len(row)):
+                print("{:d}".format(element), end="")
+                if (i != len(row) - 1):
+                    print("", end=" ")
+            print("")
