7a4dd90f571af4b916065ce80ab1c5b67cf56f2c
Blas <blashernandez98@gmail.com>
2022-09-13 11:35:33 -0700

    Task5

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -3,6 +3,6 @@ def no_c(my_string):
     if (my_string is not None):
         new_string = ""
         for letter in my_string:
-            if (letter != 'c' or letter != 'C'):
+            if (letter != 'c' and letter != 'C'):
                 new_string += letter
         return new_string
