f9faeb975e01877a2fece3af108311de0797cd0f
Blas <blashernandez98@gmail.com>
2022-09-15 15:00:44 -0700

    Task1

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    
+
     new_list = []
-    
+
     if my_list is not None:
-    
-        new_list = list(map(lambda element:\
-        replace if (element == search) else element, my_list))
-    
+
+        new_list = list(map(lambda e: replace if e == search else e, my_list))
+
     else:
         return None
 
