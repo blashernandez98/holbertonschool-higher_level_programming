ae9cdd2bba7087e3693c32b84c767c5172747435
Blas <blashernandez98@gmail.com>
2022-09-18 15:20:19 -0700

    Task2

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def safe_print_list_integers(my_list, x):
+
+    printed = 0
+
+    for i in range(x):
+
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            printed += 1
+
+        except (TypeError, ValueError):
+            continue
+    print()
+    return printed
