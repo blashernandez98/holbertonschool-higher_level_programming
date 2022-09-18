d538c1d7b54c0cbc144d3506068f703464e8bd16
Blas <blashernandez98@gmail.com>
2022-09-18 14:57:52 -0700

    Task0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -8,7 +8,7 @@ def safe_print_list(my_list, x):
         for i in range(x):
             print(my_list[i], end="")
         print()
-        return i
+        return i+1
 
     except IndexError:
         print()
