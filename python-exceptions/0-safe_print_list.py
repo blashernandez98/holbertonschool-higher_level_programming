33d28b3282edd3d39b4e1193e35d13f3f52630f6
Blas <blashernandez98@gmail.com>
2022-09-18 15:07:19 -0700

    Pycodestyle

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -5,8 +5,8 @@ def safe_print_list(my_list, x):
 
     printed = 0
 
-    for i in range(x):        
-        try:   
+    for i in range(x):
+        try:
             print(my_list[i], end="")
             printed += 1
 
