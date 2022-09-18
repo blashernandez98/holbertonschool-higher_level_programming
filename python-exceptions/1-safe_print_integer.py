9f8105ed3cc4e9eae985eee6aaa648d1452a8366
Blas <blashernandez98@gmail.com>
2022-09-18 15:13:27 -0700

    Task1

---
+++ python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def safe_print_integer(integer_maybe):
+
+    try:
+        print("{:d}".format(integer_maybe))
+        return True
+
+    except ValueError:
+        return False
