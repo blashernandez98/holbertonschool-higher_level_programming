c2cdb2bf47ef8d162486738e5cc4370dbe32997d
Blas <blashernandez98@gmail.com>
2022-09-18 18:47:01 -0700

    Task100

---
+++ python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def safe_print_integer_err(value):
+
+    import sys
+    try:
+        print("{:d}".format(value))
+        return True
+
+    except ValueError as ve:
+        print("Exception: {}".format(ve), file=sys.stderr)
+        return False
