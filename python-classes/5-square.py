27316ebb1f43f3b8c476667d23c86dea53731994
Blas <blashernandez98@gmail.com>
2022-09-20 05:53:52 -0700

    Task5

---
+++ python-classes/5-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+""" Task 1 module """
+""" Error check function for positive integers """
+
+
+def int_check(value):
+    if (type(value) != int):
+        raise TypeError("size must be an integer")
+        return False
+    if (value < 0):
+        raise ValueError("size must be >= 0")
+        return False
+    return True
+
+
+""" Square class representing a square """
+
+
+class Square():
+    """ Square class """
+    def __init__(self, size=0):
+        if (int_check(size)):
+            self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if (int_check(size)):
+            self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        size = self.size
+        if not size:
+            print()
+            return
+        for i in range(self.size):
+            for i in range(self.size):
+                print("#", end="")
+            print()
