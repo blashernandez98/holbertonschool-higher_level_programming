d9b2e303e8d0bc1f497213d99c74c2c1b50a690e
Blas <blashernandez98@gmail.com>
2022-09-25 10:04:30 -0700

    Task2

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+""" Task 2 module  """
+
+
+def int_check(value, var_name):
+    """ Type and value check for integers  """
+
+    if type(value) is not int:
+        raise TypeError(var_name + " must be an integer")
+    if value < 0:
+        raise ValueError(var_name + " must be >= 0")
+
+
+class Rectangle:
+    """ Rectangle class  """
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+
+        int_check(value, "width")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+
+        int_check(value, "height")
+        self.__height = value
+
+    def __init__(self, width=0, height=0):
+
+        int_check(width, "width")
+        int_check(height, "height")
+
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return (self.width * self.height)
+
+    def perimeter(self):
+
+        if self.width == 0 or self.height == 0:
+            return 0
+
+        return (self.width * 2 + self.height * 2)
