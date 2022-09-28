c8ac4b9f8c09fb49255fbdf06c8f22e3b8a16481
Blas <blashernandez98@gmail.com>
2022-09-28 13:30:08 -0700

    Task6

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Task 6 module """
+
+
+def load_from_json_file(filename):
+    """ Function that converts json file to python object """
+
+    import json
+
+    with open(filename, 'r', encoding="utf-8") as f:
+
+        my_str = f.reads()
+
+    return json.loads(my_str)
