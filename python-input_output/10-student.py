f440726cc91a8d220ae463d2e15a0f14f696f652
Blas <blashernandez98@gmail.com>
2022-09-28 16:56:52 -0700

    Task10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Task 10 module """
+
+
+class Student():
+    """ Student class with basic info """
+
+    def __init__(self, first, last, age):
+        """ Initializer  """
+
+        self.first_name = first
+        self.last_name = last
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ to_json function that returns json of Student """
+
+        dic = self.__dict__
+
+        if attrs is None:
+
+            return dic
+
+        new_dic = {key: dic[key] for key in attrs if key in dic}
+
+        return new_dic
