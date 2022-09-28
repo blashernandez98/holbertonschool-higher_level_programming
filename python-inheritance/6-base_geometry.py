1f83521720c503ab0e1a8b9d7962f2aa7abb7943
Blas <blashernandez98@gmail.com>
2022-09-28 11:27:36 -0700

    Task6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Task 5 module """
+
+
+class BaseGeometry():
+    """ BaseGeometry class """
+
+    def area(self):
+        """ area public instance method  """
+
+        raise Exception("area() is not implemented")
