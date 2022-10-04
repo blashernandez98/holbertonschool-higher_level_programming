7d25e77963c006b3faee9c136bbbd452c5faf4b5
Blas <blashernandez98@gmail.com>
2022-10-04 05:58:51 -0700

    Task1

---
+++ python-almost_a_circle/README.md
@@ -0,0 +1,1 @@
+# This folder contains the tasks for the "Python - almost a circle" project at Holberton School.
---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Task 0 module """
+
+
+class Base():
+    """ Base class for future classes """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ Class constructor """
+
+        if id is not None:
+            self.id = id
+
+        else:
+            __nb_objects += 1
+            self.id = __nb_objects
