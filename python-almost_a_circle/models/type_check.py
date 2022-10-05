3bd920cd5528192f1b7b3e67a6907765e4358212
Blas <blashernandez98@gmail.com>
2022-10-05 06:20:28 -0700

    Type_check module documented

--- python-almost_a_circle/models/type_check.py
+++ python-almost_a_circle/models/type_check.py
@@ -3,15 +3,16 @@
 
 
 def size_check(value, name):
-
+    """ Function that checks correct type/value for sizes """
     if type(value) is not int:
         raise TypeError(f"{name} must be an integer")
 
     if value <= 0:
         raise ValueError(f"{name} must be > 0")
 
-def pos_check(value, name):
 
+def pos_check(value, name):
+    """ Function that checks correct type/value for positions """
     if type(value) is not int:
         raise TypeError(f"{name} must be an integer")
 
