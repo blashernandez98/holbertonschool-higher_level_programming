f6f0583746abf5874a4c34a52892df756d5ea3b2
Blas <blashernandez98@gmail.com>
2022-10-05 10:47:43 -0700

    Task16

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -20,6 +20,21 @@ class Base():
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        """ Converts list of dictionaries to json string """
         if list_dictionaries is None:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_obj):
+        """ Saves list of objects to file as json string """
+        cls_name = "undefined"
+        if list_obj is None:
+            string = "[]"
+        else:
+            cls_name = type(list_obj[0]).__name__
+            dictionaries = list(map(lambda x: x.to_dictionary(), list_obj))
+            string = Base.to_json_string(dictionaries)
+
+        with open(f"{cls_name}.json", 'w', encoding="utf-8") as f:
+            f.write(string)
