58c7473952c11ae2b586e1883ddfee4eabd6a7a3
Blas <blashernandez98@gmail.com>
2022-10-06 16:12:25 -0700

    Load method

--- python-almost_a_circle/tests/test_rectangle.py
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -3,6 +3,7 @@
 import unittest
 from models.rectangle import Rectangle
 from io import StringIO
+import os
 import sys
 
 
@@ -44,17 +45,33 @@ class testRectangleClass(unittest.TestCase):
         self.assertEqual(r1.x, 3)
         self.assertEqual(r1.y, 4)
 
-    def test_rectangle_errors(self):
-        self.assertRaises(TypeError, Rectangle, "1", 2)
-        self.assertRaises(TypeError, Rectangle, 1, "2")
-        self.assertRaises(TypeError, Rectangle, 1, 2, "43")
-        self.assertRaises(TypeError, Rectangle, 1, 2, 3, "43")
-        self.assertRaises(ValueError, Rectangle, -1, 2)
-        self.assertRaises(ValueError, Rectangle, 1, -2)
-        self.assertRaises(ValueError, Rectangle, 0, 2)
-        self.assertRaises(ValueError, Rectangle, 1, 0)
-        self.assertRaises(ValueError, Rectangle, 1, 2, -3)
-        self.assertRaises(ValueError, Rectangle, 1, 2, 3, -4)
+    def test_rectangle_save(self):
+        Rectangle.save_to_file(None)
+        with open("Rectangle.json") as f:
+            self.assertEqual(f.read(), '[]')
+
+        Rectangle.save_to_file([])
+        with open("Rectangle.json") as f:
+            self.assertEqual(f.read(), '[]')
+
+        r1 = Rectangle(1, 2, 0, 0, 43)
+        Rectangle.save_to_file([r1])
+        with open("Rectangle.json") as f:
+            expected = '[{"id": 43, "width": 1, "height": 2, "x": 0, "y": 0}]'
+            self.assertEqual(f.read(), expected)
+
+    def test_rectangle_load(self):
+        if os.path.exists("Rectangle.json"):
+            os.remove("Rectangle.json")
+        output = Rectangle.load_from_file()
+        self.assertEqual(output, [])
+
+    def test_rectangle_load2(self):
+        r1 = Rectangle(2, 2)
+        r1.update(43)
+        r1.save_to_file([r1])
+        output = Rectangle.load_from_file()
+        self.assertEqual(output[0].id, 43)
 
     def test_rectangle_display(self):
         captured_out = StringIO()
@@ -71,3 +88,15 @@ class testRectangleClass(unittest.TestCase):
         r_with_pos.display()
         self.assertEqual(captured_out.getvalue(), '\n #\n')
         sys.stdout = sys.__stdout__
+
+    def test_rectangle_errors(self):
+        self.assertRaises(TypeError, Rectangle, "1", 2)
+        self.assertRaises(TypeError, Rectangle, 1, "2")
+        self.assertRaises(TypeError, Rectangle, 1, 2, "43")
+        self.assertRaises(TypeError, Rectangle, 1, 2, 3, "43")
+        self.assertRaises(ValueError, Rectangle, -1, 2)
+        self.assertRaises(ValueError, Rectangle, 1, -2)
+        self.assertRaises(ValueError, Rectangle, 0, 2)
+        self.assertRaises(ValueError, Rectangle, 1, 0)
+        self.assertRaises(ValueError, Rectangle, 1, 2, -3)
+        self.assertRaises(ValueError, Rectangle, 1, 2, 3, -4)
