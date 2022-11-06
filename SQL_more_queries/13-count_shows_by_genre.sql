07c5aab869f8b6cfb7bba9dd3c91ce5c1b13ed03
blas <blashernandez98@gmail.com>
2022-11-06 19:43:02 -0300

    All caps

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- Task 13
-SELECT name as genre, COUNT(tv_show_genres.genre_id) as number_of_shows
+SELECT name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
 FROM tv_genres
 JOIN tv_show_genres
 ON tv_genres.id=tv_show_genres.genre_id
