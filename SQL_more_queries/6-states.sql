fca32d176b7fb578bb477e6eed0f0e8958fee1a4
blas <blashernandez98@gmail.com>
2022-11-06 17:37:19 -0300

    Task 6

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- Task 6
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+    id INT UNIQUE NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256),
+    PRIMARY KEY (id)
+);
