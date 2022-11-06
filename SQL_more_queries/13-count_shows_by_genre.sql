39a482eecca5fe1ea5068736e2a05f3b5d128bc6
blas <blashernandez98@gmail.com>
2022-11-06 19:42:09 -0300

    Task 13

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- Task 13
+SELECT name as genre, COUNT(tv_show_genres.genre_id) as number_of_shows
+FROM tv_genres
+JOIN tv_show_genres
+ON tv_genres.id=tv_show_genres.genre_id
+GROUP BY tv_show_genres.genre_id;
