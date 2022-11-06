3832a94cfb905a3bcc85bf3ede6e4f71a1170786
blas <blashernandez98@gmail.com>
2022-11-06 20:46:11 -0300

    Considered null genre

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,8 +1,8 @@
 -- Task 16
 SELECT s.title, g.name
 FROM tv_shows s
-JOIN tv_show_genres sg
+LEFT JOIN tv_show_genres sg
 ON s.id=sg.show_id
-JOIN tv_genres g
+LEFT JOIN tv_genres g
 ON sg.genre_id=g.id
 ORDER BY s.title, g.name;
