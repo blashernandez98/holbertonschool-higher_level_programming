c9bcf0c34b5ac5b56e8c75243aa87f7a00e5189a
blas <blashernandez98@gmail.com>
2022-11-08 16:57:22 -0300

    Task 1

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Task 1 module """
+
+
+import MySQLdb
+import sys
+
+args = sys.argv
+if len(args) == 4:
+    user = args[1]
+    passwd = args[2]
+    datab = args[3]
+
+    db = MySQLdb.connect(host='localhost', user=user, passwd=passwd, db=datab)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id DESC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
