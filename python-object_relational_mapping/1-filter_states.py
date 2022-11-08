1ed13382823b4a1bc4df6a6e4f5d8e8b0e0dbdf4
blas <blashernandez98@gmail.com>
2022-11-08 17:07:58 -0300

    Case sensitive search

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -13,7 +13,7 @@ if len(args) == 4:
 
     db = MySQLdb.connect(host='localhost', user=user, passwd=passwd, db=datab)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id DESC")
+    cur.execute("SELECT * FROM states WHERE name REGEXP '^[N].*$' ORDER BY id DESC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
