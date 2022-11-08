b845fe4bbc7f3bdbf92078aa0491a3befc5e8888
blas <blashernandez98@gmail.com>
2022-11-08 19:11:27 -0300

    Task 1 fixed docs

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -6,17 +6,17 @@ import MySQLdb
 import sys
 
 args = sys.argv
-if len(args) != 4:
-    exit()
-user = args[1]
-passwd = args[2]
-datab = args[3]
-db = MySQLdb.connect(host='localhost', user=user, passwd=passwd, db=datab)
-cur = db.cursor()
-cur.execute(
-    "SELECT * FROM states\
-    WHERE ASCII(left(name, 1)) between ASCII('N') and ASCII('N')\
-    ORDER BY id")
-rows = cur.fetchall()
-for row in rows:
-    print(row)
+if len(args) == 4:
+    user = args[1]
+    passwd = args[2]
+    datab = args[3]
+    db = MySQLdb.connect(host='localhost', user=user, passwd=passwd, db=datab)
+    cur = db.cursor()
+    cur.execute(
+        "SELECT * FROM states\
+        WHERE ASCII(left(name, 1)) between ASCII('N') and ASCII('N')\
+        ORDER BY id"
+    )
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
