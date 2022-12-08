82598dd21832c0aa45b6ff978ec456bccb3c545c
blas <blashernandez98@gmail.com>
2022-12-08 11:53:45 -0300

    Task 11

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,13 +1,18 @@
 #!/usr/bin/node
 
 let big = 0;
+let second = 0;
 const args = process.argv.slice(2);
 if (args.length > 1) {
   args.forEach(e => {
-    if (Number(e) > big) {
-      big = Number(e);
+    e = Number(e);
+    if (e > big) {
+      second = big;
+      big = e;
+    } else if (e > second) {
+      second = e;
     }
   });
 }
 
-console.log(big);
+console.log(second);
