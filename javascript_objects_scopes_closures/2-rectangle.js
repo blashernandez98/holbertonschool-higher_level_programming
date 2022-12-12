5c82be67d5efb9a3b8ebc488d5de308273da2b8b
blas <blashernandez98@gmail.com>
2022-12-11 23:03:25 -0300

    Task 2

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0) {
+      this.width = w;
+    }
+    if (h > 0) {
+      this.height = h;
+    }
+  }
+}
+
+module.exports = Rectangle;
