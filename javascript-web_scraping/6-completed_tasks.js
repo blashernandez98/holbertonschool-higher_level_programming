4ce4265a758e1fdc67a0d68660098883716ce470
blas <blashernandez98@gmail.com>
2022-12-13 19:02:16 -0300

    Task 6

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const request = require('request');
+const urlPath = process.argv[2];
+const complete = {};
+
+request(urlPath, (err, res, body) => {
+  if (err) throw (err);
+  const ans = JSON.parse(body);
+  ans.forEach(task => {
+    if (task.completed) {
+      const user = task.userId;
+      if (user in complete) {
+        complete[user]++;
+      } else {
+        complete[user] = 1;
+      }
+    }
+  });
+  console.log(complete);
+});
