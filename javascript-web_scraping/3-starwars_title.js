6061c898df1c372cd03e7dc929999cd6eb198700
blas <blashernandez98@gmail.com>
2022-12-13 15:48:23 -0300

    Task 3

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const movieNumber = process.argv[2];
+const urlPath = 'https://swapi-api.hbtn.io/api/films/' + movieNumber;
+const request = require('request');
+
+request(urlPath, function (err, res, body) {
+  if (err) throw (err);
+  console.log(JSON.parse(body).title);
+});
